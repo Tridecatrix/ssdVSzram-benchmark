a0ca137eef44d24fc8af3c946a2fc9aeaa2f72db
u7300623 <u7300623@anu.edu.au>
2025-02-25 18:14:52 +1100

    upgraded run script

--- run.sh
+++ run.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-# run this script with command ./run.sh > data/log.txt &
+# run this script with command ./run.sh > data/log.txt & 2>&1
 
-./system_util/start_statistics.sh -d data/system-util
+if [[ $# -gt 1 ]]; then
+    # expname is name of experiment, which is appended to the result directory name created in directory data
+    echo "Usage: ./run.sh expname"
+fi
+
+if [[ -z $1 ]]; then
+    EXPNAME=unnamed
+else
+    EXPNAME=$1
+fi
+
+RESULTSDIR=data/$(date +%F-time-%H-%M-%S)-$EXPNAME
+
+mkdir -p $RESULTSDIR
+mkdir $RESULTSDIR/fio
+mkdir $RESULTSDIR/system-util
+
+./system_util/start_statistics.sh -d $RESULTSDIR/system-util
 echo "statistics started"
 
-#fio config/job-file-async.ini --output=data/fio/log.txt
+#fio config/job-file-async.ini --output=$RESULTSDIR/fio/log.txt
+#echo "fio started"
 sleep 1
 
-./system_util/stop_statistics.sh -d data/system-util
+./system_util/stop_statistics.sh -d $RESULTSDIR/system-util
 echo "statistics ended"
 
-./system_util/extract-data.sh -r data/system-util -d /dev/zram0 -d /dev/nvme0n1
+./system_util/extract-data.sh -r $RESULTSDIR/system-util -d zram0 -d nvme0n1
 echo "data extracted"
